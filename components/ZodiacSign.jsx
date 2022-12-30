013cbd875f71aa12c7c3690397ab1fabf8bd219c
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-12-30 04:01:03 +0300

    adding images

--- components/ZodiacSign.jsx
+++ components/ZodiacSign.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import leo from "../assets/leosign.jpg";
+import Image from "next/image";
 
-const ZodiacSign = () => {
-  return <div>ZodiacSign</div>;
+const ZodiacSign = ({ image }) => {
+  const images = {
+    leo: leo,
+    // add more images here
+  };
+  const src = images[image];
+  console.log(image);
+  return (
+    <div>
+      <Image src={src} alt="leo" width={200} height={200} />
+    </div>
+  );
 };
 
 export default ZodiacSign;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+import ZodiacSign from "../components/ZodiacSign";
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -15,8 +15,22 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}></main>
+      <div class="d-flex flex-wrap bd-highlight mb-3">
+        <div class="p-2 bd-highlight">
+          {" "}
+          <ZodiacSign image="leo" /> 1
+        </div>
+        <div class="p-2 bd-highlight">
+          {" "}
+          <ZodiacSign image="leo" /> 1
+        </div>
+        <div class="p-2 bd-highlight">
+          {" "}
+          <ZodiacSign image="leo" /> 1
+        </div>
+      </div>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -27,7 +41,7 @@ export default function Home() {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
