7639f5207846c01366a8777c51b8df197b4b070a
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-01-21 06:25:02 +0300

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,10 @@ import Header from "../components/Header";
 import ActionCall from "../components/ActionCall";
 import Form from "../components/Form";
 import EmailExample from "../components/EmailExample";
+import ReactGA from "react-ga4";
+
+//Initialize GA4
+ReactGA.initialize("G-N2MLJ5SHN5");
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -21,7 +25,6 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
       <ActionCall />
       <EmailExample />
-
       <Form />
       <footer className="footer">
         <p> 2023 Zodiac AI. All rights reserved.</p>{" "}
@@ -30,5 +33,14 @@ function MyApp({ Component, pageProps }) {
     </main>
   );
 }
+const SendAnalytics = () => {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}; // If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(SendAnalytics);
 
 export default MyApp;
